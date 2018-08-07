64eaf28e4281ccca88e8bf45d885235f0b9d4039
frolsen <frolsen@gmail.com>
2018-08-07 15:16:30 +0200

    Added calculations

--- HelloWorld.go
+++ HelloWorld.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
    fmt.Println("Hello, World!")
+
+   var x float64
+   x = 20.0
+   fmt.Println(x)
+   fmt.Printf("x is of type %T\n", x)
+   
+   var y = 3;
+
+   fmt.Printf("y is of type %T\n", y)
+
 }
